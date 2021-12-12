15787d026fa9d9e1d91a7c29a102142806894d99
heynar-max <heynar76@hotmail.com>
2021-12-12 10:30:02 -0500

    colores y funcionalidad Navbar

--- portafolio/src/Styles/Globalstyle.jsx
+++ portafolio/src/Styles/Globalstyle.jsx
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+:root{
+    --color-fondo: #000000;
+}
 
 *{
     padding: 0;
     margin: 0;
     
+}
+
+body{
+    background-color: var(--color-fondo);
 }
 `;
 
---
+++ portafolio/src/Styles/Icons.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Icon = styled.i`
+    font-size: 25px;
+    margin-right: 5px;
+`;
